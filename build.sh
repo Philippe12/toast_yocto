100f352f597a046fddd63eaefefc5ee71d61ee68
Philippe FOUQUET <ph.fouquet@gmail.com>
2020-01-08 14:17:19 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TARGET=/mnt
+BUILD=/root/rpi/build
+
+#mkdir -p $BUILD/conf
+source /root/poky-morty/oe-init-build-env $BUILD
+
+#if [ -f $TARGET/local.conf ]; then
+#  cp -f $TARGET/local.conf $BUILD/conf/local.conf
+#else
+#  cp -f /root/rpi/meta-rpi/conf/local.conf.sample $BUILD/conf/local.conf
+#fi
+
+#if [ -f $TARGET/bblayers.conf ]; then
+#  cp -f $TARGET/bblayers.conf $BUILD/conf/bblayers.conf
+#else
+#  cp -f /root/rpi/meta-rpi/conf/bblayers.conf.sample $BUILD/conf/bblayers.conf
+#fi
+
+#touch $BUILD/conf/sanity.conf
+cd $BUILD
+source toaster start webport="0.0.0.0:8000"
+#bitbake console-image
+#cp -r $BUILD/tmp/deploy/images/* $TARGET/
