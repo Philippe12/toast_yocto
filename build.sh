4ab2c10adaeb14bcc957bd2aca2256a71cf4d6c9
Philippe FOUQUET <ph.fouquet@gmail.com>
2020-01-09 14:38:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /root/poky/oe-init-build-env /mnt
+source /home/builderuser/poky/oe-init-build-env /mnt
 
 source toaster start webport="0.0.0.0:8000" toasterdir="/mnt"
 while true; do $(echo date); sleep 120; done
