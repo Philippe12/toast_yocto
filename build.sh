6a68883b9e002ff4fe37d37ef6dac1f9956b9aeb
Philippe FOUQUET <ph.fouquet@gmail.com>
2020-01-16 12:23:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source /home/builderuser/poky/oe-init-build-env /mnt
+source /home/builderuser/poky/oe-init-build-env /working
 
-source toaster start webport="0.0.0.0:8000" toasterdir="/mnt"
+source toaster start webport="0.0.0.0:8000" toasterdir="/working"
 /home/builderuser/poky/bitbake/lib/toaster/manage.py makemigrations
 /home/builderuser/poky/bitbake/lib/toaster/manage.py migrate
 source toaster stop
 
-source toaster start webport="0.0.0.0:8000" toasterdir="/mnt"
+source toaster start webport="0.0.0.0:8000" toasterdir="/working"
 while true; do $(echo date); sleep 120; done
